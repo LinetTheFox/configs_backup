bbe7501d4179688217fa2a9bcdc4ccc790eee484
LinetTheFox <linetmoreandil@gmail.com>
2021-08-09 02:06:02 +0300

    20210809020602

---
+++ util/battery_check.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Note - my laptop I use, ThinkPad t440s, uses two batteries, so the critical value is considered to be
+# the average less than 5.
+
+# Ignore if we are connected to AC
+skip_check=$(cat /sys/class/power_supply/AC/online)
+
+if [ ! $skip_check ]; then
+	bat0=$(cat /sys/class/power_supply/BAT0/capacity)
+	bat1=$(cat /sys/class/power_supply/BAT1/capacity)
+	cap=$(( ( $bat0 + $bat1 ) / 2 ))
+	[ $cap -lt 10  ] && notify-send --urgency=critical 'Battery Low'
+fi
